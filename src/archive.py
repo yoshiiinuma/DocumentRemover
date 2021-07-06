7773c4701dcfac8f0fd4cba3b6faaf4899a4439e
yoshiiinuma <iinumayoshiaki@gmail.com>
2021-07-06 10:47:57 -1000

    Create new function delete_folder_by_current_date from delete_folder

--- src/archive.py
+++ src/archive.py
@@ -4,6 +4,8 @@ Archive Manager
 #pylint: disable=invalid-name
 import re
 import time
+from datetime import datetime
+from datetime import timedelta
 from datetime import date
 import src.config as Config
 from src.drive import Drive
@@ -42,16 +44,16 @@ class Archive:
             if not requests:
                 self._close()
                 return 0
-            cnt = len(requests)
+            #cnt = len(requests)
             fmap = DB.conv_requests_to_map(requests)
             names = fmap.keys()
             files = self.drive.get_files_by_name(names)
-            rslt = self.db.populate_archive_file_info(files, fmap)
-            print(f'ARCHIVE.POPULATE_FILE_INFO: {rslt} Populated')
-            rslt = self.db.complete_file_info_population()
-            print(f'ARCHIVE.POPULATE_FILE_INFO: {rslt} Completed')
+            cnt_files = self.db.populate_archive_file_info(files, fmap)
+            print(f'ARCHIVE.POPULATE_FILE_INFO: {cnt_files} ArchivedFile Populated')
+            cnt_reqs = self.db.complete_file_info_population()
+            print(f'ARCHIVE.POPULATE_FILE_INFO: {cnt_reqs} ArchivedRequest Updated')
             self._close()
-            return cnt
+            return cnt_files if cnt_files else 0
         except Exception as err:
             print(err)
             self._close()
@@ -118,60 +120,85 @@ class Archive:
         """
         Moves files to Archive folder
         Sets 2 to ArchivedFiles.Status
-        Sets 4 to ArchivedRequests.Status if all the dependent files have Status 2
+        Sets 5 to ArchivedRequests.Status if all the dependent files have Status 2
 
         INPUT
             archive_date (String): the date that the files are archived; yyyymmdd
         """
+        print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: {archive_date}')
         if not archive_date:
             archive_date = date.today().strftime('%Y%m%d')
         if not re.match(r'^20\d{2}\d{2}\d{2}$', archive_date):
-            print('MOVE_FILES_TO_ARCHIVE_DIR: Invalid Date ' + archive_date)
+            print('ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: Invalid Date ' + archive_date)
             return 0
-        try:
-            cnt = 0
-            self._open()
-            rslt = self.drive.create_folder(archive_date, self.conf['DRIVE_DELETED_FILES'])
-            dst_folder_id = rslt['id']
-            while True:
-                rslt = self.db.get_requests_to_archive()
-                if not rslt:
-                    print('ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: NO MORE DATA')
-                    break
-                files = Archive.conv_to_moving_files(rslt)
-                limit = len(files)
-                print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: PROCESSING ({limit})')
-                index = 0
-                size = 50
-                while index < limit:
-                    batch_rslt = self.drive.batch_move(files[index:index+size], dst_folder_id)
-                    #print(batch_rslt)
-                    rslt = self.db.set_archived_flag_to_files(batch_rslt, archive_date)
-                    cnt += rslt
-                    index += size
-                    print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: CUR {rslt} / TOTAL {cnt} Processed')
-                    time.sleep(1)
-            rslt = self.db.complete_file_archive(dst_folder_id, archive_date)
-            print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: {rslt} Requests Processed')
-            print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: {cnt} Files Moved')
-            self._close()
-            return cnt
-        except Exception as err:
-            print(err)
-            return 0
+        #try:
+        cnt = 0
+        self._open()
+        rslt = self.drive.create_folder(archive_date, self.conf['DRIVE_DELETED_FILES'])
+        dst_folder_id = rslt['id']
+        while True:
+            rslt = self.db.get_requests_to_archive()
+            if not rslt:
+                print('ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: NO MORE DATA')
+                break
+            files = Archive.conv_to_moving_files(rslt)
+            limit = len(files)
+            print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: PROCESSING ({limit})')
+            index = 0
+            size = 50
+            while index < limit:
+                batch_rslt = self.drive.batch_move(files[index:index+size], dst_folder_id)
+                rslt = self.db.set_archived_flag_to_files(batch_rslt, archive_date)
+                cnt += rslt
+                index += size
+                print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: CUR {rslt} / TOTAL {cnt} Processed')
+                time.sleep(1)
+        rslt = self.db.complete_file_archive(dst_folder_id, archive_date)
+        print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: {rslt} Requests Processed')
+        print(f'ARCHIVE.MOVE_FILES_TO_ARCHIVE_DIR: {cnt} Files Moved')
+        self._close()
+        return cnt
+        #except Exception as err:
+        #    print(err)
+        #    return 0
 
-    def delete_folder(self, archive_date):
+    def delete_folder_by_current_date(self, current_date = None, retention_period = 30):
+        """
+        Delete folder archived on the specified date under DELETE folder
+        Sets 6 to ArchivedRequests.Status
+
+        INPUT
+            current_date (String): the date on which folder is deleted; yyyymmdd
+        """
+        if not current_date:
+            print('ARCHIVE.DELETE_FOLDER_BY_CURRENT_DATE: No Current Date Provided')
+            return None
+        if not re.match(r'^20\d{2}\d{2}\d{2}$', current_date):
+            print('ARCHIVE.DELETE_FOLDER_BY_CURRENT_DATE: Invalid Date ' + current_date)
+            return None
+        archive_date = (
+            datetime.strptime(current_date, '%Y%m%d') - timedelta(retention_period)
+        ).strftime('%Y%m%d') 
+        return self.delete_folder(archive_date, current_date)
+
+    def delete_folder(self, archive_date, delete_date = None):
         """
         Delete folder archived on the specified date under DELETE folder
         Sets 6 to ArchivedRequests.Status
 
         INPUT
             archive_date (String): the date that the files are archived; yyyymmdd
+            delete_date (String): the date that the files are deleted; yyyymmdd
         """
+        print(f'ARCHIVE.DELETE_FOLDER: DATE ARCHIVE {archive_date}, DELETE {delete_date}')
         if not archive_date:
-            archive_date = date.today().strftime('%Y%m%d')
+            print('ARCHIVE.DELETE_FOLDER: No Archive Date Provided')
+            return None
         if not re.match(r'^20\d{2}\d{2}\d{2}$', archive_date):
-            print('MOVE_FILES_TO_ARCHIVE_DIR: Invalid Date ' + archive_date)
+            print('ARCHIVE.DELETE_FOLDER: Invalid Archive Date ' + archive_date)
+            return None
+        if delete_date and not re.match(r'^20\d{2}\d{2}\d{2}$', delete_date):
+            print('ARCHIVE.DELETE_FOLDER: Invalid Delete Date ' + delete_date)
             return None
         try:
             self._open()
@@ -181,8 +208,8 @@ class Archive:
                 return None
             folder_id = rslt['id']
             rslt = self.drive.delete_folder(folder_id)
-            print('ARCHIVE.DELETE_FOLDER: Folder Deleted ' + archive_date)
-            rslt = self.db.complete_file_delete()
+            print(f'ARCHIVE.DELETE_FOLDER: Folder {archive_date} Deleted on {delete_date}')
+            rslt = self.db.complete_file_delete(archive_date, delete_date)
             print('ARCHIVE.DELETE_FOLDER: Completed Set 6 to ArchivedRequests.Status')
             print(f'{rslt} Requests Processed')
             self._close()
@@ -268,6 +295,27 @@ class Archive:
             print(f'{rslt} Travelers Restored')
             rslt = self.db.undone_request_file_path(data['request_files'])
             print(f'{rslt} Requests Restored')
+            return True
+        except Exception as err:
+            print(err)
+            return err
+
+    def check_db_status(self, target_date):
+        """
+        Checks if the DB archive status at the given date is valid
+
+        INPUT
+            target_date (String): assumed to be the current date in YYYYMMDD
+        """
+        if not target_date:
+            target_date = date.today().strftime('%Y%m%d')
+        elif not re.match(r'^20\d{2}\d{2}\d{2}$', target_date):
+            print('CHECK_DB_STATUS: Invalid Date ' + target_date)
+            return None
+        target_date = re.sub(r'^(20\d{2})(\d{2})(\d{2})$', r'\1-\2-\3', target_date)
+        try:
+            self._open()
+            self._close()
             return True
         except Exception as err:
             print(err)
