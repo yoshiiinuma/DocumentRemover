ad46e54cdd130960f62c1e7a48aa96b5ac688394
yoshiiinuma <iinumayoshiaki@gmail.com>
2021-07-06 10:35:24 -1000

    Rename bin/run_stored_proc.py to bin/run_stored_procs.py

--- bin/run_stored_procs.py
+++ bin/run_stored_procs.py
@@ -3,11 +3,18 @@ Run Stored Procedures
 """
 #pylint: disable=wrong-import-position,import-error,invalid-name
 import argparse
+import re
+from datetime import datetime
 from os.path import dirname, abspath, exists
 import sys
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 import src.config as Config
 from src.db import DB
+
+def check_date(value):
+    if not re.match(r'^20\d{6}$', value):
+        raise argparse.ArgumentTypeError('Current Date must be YYYYMMDD: ' + value)
+    return re.sub(r'^(20\d{2})(\d{2})(\d{2})$', r'\1-\2-\3', value)
 
 def get_argparser():
     """
@@ -19,6 +26,7 @@ def get_argparser():
                         help='retention period for regular requests until archive')
     parser.add_argument('days2', default=240, type=int,
                         help='retention period for irregular requests until archive')
+    parser.add_argument('current_date', type=check_date, help='assumed to be the current date (YYYYMMDD)')
     return parser
 
 def main():
@@ -30,13 +38,14 @@ def main():
         print('ENV File Found: ' + args.envfile)
     else:
         print('ENV File Not Found: ' + args.envfile)
+    print(args)
     conf = Config.load(args.envfile)
     db = DB(conf)
     db.connect()
-    rslt = db.exec(f'CALL CreateArchiveRequests({args.days1})')
+    rslt = db.exec(f"CALL CreateArchiveRequests({args.days1}, '{args.current_date}')")
     print('CreateArchiveRequests Called')
     print(f'{rslt} ArchivedRequests Created')
-    rslt = db.exec(f'CALL CreateArchiveRequestsWithInvalidDate({args.days2})')
+    rslt = db.exec(f"CALL CreateArchiveRequestsWithInvalidDate({args.days2}, '{args.current_date}')")
     print('CreateArchiveRequestsWithInvalidDate Called')
     print(f'{rslt} ArchivedRequests With Invalid Date Created')
     rslt = db.exec('CALL CreateArchiveFiles()')
